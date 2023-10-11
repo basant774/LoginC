99f7b497d92f478f49b0e2664c41fa369d2080e7
Your Name <you@example.com>
2023-10-12 01:48:58 +0300

    First commit

--- test.c
+++ test.c
@@ -1,22 +1,87 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
-int main() {
-    char username[40];
-    char password[20];
+#define MAX_USERS 10
+#define MAX_USERNAME_LENGTH 20
+#define MAX_PASSWORD_LENGTH 20
 
-    printf("Enter Your username: ");
+typedef struct {
+    char username[MAX_USERNAME_LENGTH];
+    char password[MAX_PASSWORD_LENGTH];
+} User;
+
+User users[MAX_USERS];
+int totalUsers = 0;
+
+void registerUser() {
+    char username[MAX_USERNAME_LENGTH];
+    char password[MAX_PASSWORD_LENGTH];
+
+    printf("Please enter your username: ");
     scanf("%s", username);
 
-    printf("Enter Your password: ");
+    printf("Please enter your password: ");
     scanf("%s", password);
 
-    if (strcmp(username, "basant") == 0 && strcmp(password, "1234") == 0) {
-        printf("Success Login\n");
-    } else {
-        printf("Failure Login\n");
+    if (totalUsers >= MAX_USERS) {
+        printf("Cannot register more users.\n");
+        return;
     }
 
-    return 0;
+    strcpy(users[totalUsers].username, username);
+    strcpy(users[totalUsers].password, password);
+    totalUsers++;
+
+    printf("User registered successfully.\n");
+}
+
+void loginUser() {
+    char username[MAX_USERNAME_LENGTH];
+    char password[MAX_PASSWORD_LENGTH];
+
+    printf("Please enter your username: ");
+    scanf("%s", username);
+
+    printf("Please enter your password: ");
+    scanf("%s", password);
+
+    int loggedIn = 0;
+
+    for (int i = 0; i < totalUsers; i++) {
+        if (strcmp(users[i].username, username) == 0 && strcmp(users[i].password, password) == 0) {
+            printf("Login successful.\n");
+            loggedIn = 1;
+            break;
+        }
+    }
+
+    if (!loggedIn) {
+        printf("Login failed.\n");
+    }
+}
+
+int main() {
+    int choice;
+
+    while (1) {
+        printf("\n1. Register\n");
+        printf("2. Login\n");
+        printf("3. Exit\n");
+        printf("Please enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                registerUser();
+                break;
+            case 2:
+                loginUser();
+                break;
+            case 3:
+                printf("Exiting program.\n");
+                return 0;
+            default:
+                printf("Invalid choice. Please try again.\n");
+        }
+    }
 }
